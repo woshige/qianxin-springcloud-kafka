be234d35c458d500d4bbf2513b3f63eb41689844
unknown <sunlichao@ESG.360ES.CN>
2020-01-14 16:31:39 +0800

    'add'

--- server1/src/main/java/com/qianxin/service/SendService.java
+++ server1/src/main/java/com/qianxin/service/SendService.java
@@ -2,6 +2,7 @@ package com.qianxin.service;
 
 
 import com.qianxin.enums.ResultEnum;
+import com.qianxin.listener.KafKaProducerListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Service;
 public class SendService {
 
     @Autowired
-    private KafkaTemplate<String, Object> template;
+    private KafkaTemplate<String, String> template;
+    @Autowired
+    private KafKaProducerListener kafKaProducerListener;
 
     public ResultEnum send(String key,String entity) {
+        template.setProducerListener(kafKaProducerListener);
         template.send(key, String.valueOf(System.currentTimeMillis()), entity);
         return ResultEnum.SUCCESS;
     }
